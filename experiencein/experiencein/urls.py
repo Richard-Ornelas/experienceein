7093d19310d74ba7985b8750f5d1e913ea4f73ce
Aluno <Richard Ornelas>
2022-10-04 14:04:19 -0400

    Incluindo o HTML, as rotas e a parte do banco de dados

--- experiencein/experiencein/urls.py
+++ experiencein/experiencein/urls.py
@@ -1,9 +1,7 @@
-
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
-from perfis import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
+    path('admin/',admin.site.urls), 
+    path('', include('perfis.urls'))
 ]
